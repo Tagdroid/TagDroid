f99500b15885fc69ee939fadf867b3179852e59d
Félix Piédallu <felix@piedallu.me>
2014-10-14 17:19:12 +0200

    Modifié un peu ton code dans disruption (l'ordre en fait) Ajouté les Disruption Array poru les lignes par exemple.

--- src/main/java/com/tagdroid/tagapi/JSonApi/Disruption/DisruptionType.java
+++ src/main/java/com/tagdroid/tagapi/JSonApi/Disruption/DisruptionType.java
@@ -3,35 +3,41 @@ package com.tagdroid.tagapi.JSonApi.Disruption;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+/*
+    Pour plus de cohérence et pour ne pas risquer de ses planter, ça peut être mieux de mettre
+    les arguments dans l'ordre alphabétique… Sinon on risque d'avoir des conflits ^^''
+    (C'est une norme comme une autre quoi…)
+ */
+
+// J'ai modifié ton code pour avoir un ordre alphabétique ;) Tu peux virer ce comm dès que tu l'as lu !
+
 public class DisruptionType {
+    private Integer Code;
+    private Integer Id;
     private String Name;
-    private Integer Code,
-            Id;
 
 
     public DisruptionType(JSONObject jsonDisruptionType) throws JSONException {
-        this.Name = jsonDisruptionType.getString("Name");
         this.Code = jsonDisruptionType.getInt("Code");
         this.Id = jsonDisruptionType.getInt("Id");
+        this.Name = jsonDisruptionType.getString("Name");
     }
 
-    public DisruptionType(String Name,
-                          Integer Code,
-                          Integer Id) {
-
-        this.Name = Name;
+    public DisruptionType(Integer Code,
+                          Integer Id,
+                          String Name) {
         this.Code = Code;
         this.Id = Id;
+        this.Name = Name;
     }
 
-    public String getName() {
-        return Name;
-    }
     public Integer getCode() {
         return Code;
     }
     public Integer getId() {
         return Id;
     }
-
+    public String getName() {
+        return Name;
+    }
 }
