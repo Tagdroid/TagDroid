676123888ff7767cde7e47766df4e0e642cda52e
Félix Piédallu <felix@piedallu.me>
2014-11-22 12:02:13 +0100

    added some in gitignore

--- app/src/main/java/com/tagdroid/android/Welcome/WelcomeActivity.java
+++ app/src/main/java/com/tagdroid/android/Welcome/WelcomeActivity.java
@@ -14,7 +14,6 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
-import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.gms.common.GooglePlayServicesUtil;
