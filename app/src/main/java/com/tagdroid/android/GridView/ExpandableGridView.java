6a64a45bbc5194b72e06e2ae53eb7253a27ca26b
Félix Piédallu <felix@piedallu.me>
2015-12-15 23:40:32 +0100

    simpler code

--- app/src/main/java/com/tagdroid/android/GridView/ExpandableGridView.java
+++ app/src/main/java/com/tagdroid/android/GridView/ExpandableGridView.java
@@ -21,14 +21,12 @@ public class ExpandableGridView extends GridView {
 
     @Override
     public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-
         // Calculate entire height by providing a very large height hint.
         // View.MEASURED_SIZE_MASK represents the largest height possible.
-        int expandSpec = MeasureSpec.makeMeasureSpec(MEASURED_SIZE_MASK, MeasureSpec.AT_MOST);
-        super.onMeasure(widthMeasureSpec, expandSpec);
-
-        ViewGroup.LayoutParams params = getLayoutParams();
-        params.height = getMeasuredHeight();
+        super.onMeasure(
+                widthMeasureSpec,
+                MeasureSpec.makeMeasureSpec(MEASURED_SIZE_MASK, MeasureSpec.AT_MOST)
+        );
+        getLayoutParams().height = getMeasuredHeight();
     }
-
 }
